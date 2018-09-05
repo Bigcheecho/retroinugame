7c3bf2ccafdab4cf69f7fd0803d3fe8e7f24268d
Jared <jaredhansen2007@yahoo.com>
2018-09-05 11:50:45 -0700

    Added enemy time freeze, fixed enemy damage

--- Assets/scripts/enemyDamage.cs
+++ Assets/scripts/enemyDamage.cs
@@ -8,6 +8,7 @@ public class enemyDamage : MonoBehaviour
 	// initial variables
 	public int enemyHealth;
 	//private Collider2D enemyCollider;
+	private bool playerInTrigger = false;
 
 	// Use this for initialization
 	void Start () 
@@ -20,20 +21,33 @@ public class enemyDamage : MonoBehaviour
 	// Update is called once per fixed time interval
 	void FixedUpdate () 
 	{
-		if(enemyHealth <= 0) 
+		if(enemyHealth <= 0 && !timeFreeze.timeFrozen) 
 		{
 			// death sequence
 			Destroy(gameObject);
 		}
+		if (Input.GetButtonDown ("Fire3") && playerInTrigger && !timeFreeze.timeFrozen) {
+			// reduces enemy health by 1
+			enemyHealth--;
+		}
 	}
 	
 	// Update is called while things are in the collider
-	void OnTriggerStay2D(Collider2D enemyCollider) 
+	/* void OnTriggerStay2D(Collider2D enemyCollider) 
 	{
 		// checks if player is attacking
 		if (Input.GetButtonDown ("Fire3") && enemyCollider.gameObject.tag == "Player" && !timeFreeze.timeFrozen) {
 			// reduces enemy health by 1
 			enemyHealth--;
-		}
+		} // reset mechanism for pressing attack 
+	} */
+
+	void OnTriggerEnter2D (Collider2D enemyCollider)
+	{
+		if (enemyCollider.gameObject.tag == "Player") playerInTrigger = true;
+	}
+	void OnTriggerExit2D (Collider2D enemyCollider)
+	{
+		if (enemyCollider.gameObject.tag == "Player") playerInTrigger = false;
 	}
 }
---
+++ Assets/scripts/enemyTimeFreeze.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyTimeFreeze : MonoBehaviour {
+	// initial variables
+	private Rigidbody2D enemyRB;
+	private bool timeFreezeInitialization;
+	private float verticalVelocity;
+	private float horizontalVelocity;
+
+	// Use this for initialization
+	void Start () {
+		// sets enemyRB to the enemy's rigidbody
+		enemyRB = GetComponent<Rigidbody2D> ();
+	}
+	
+	// FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+	void FixedUpdate () {
+		// when time is frozen
+		if (timeFreeze.timeFrozen && !timeFreezeInitialization) 
+		{
+			// stores former velocities in freeze
+			verticalVelocity = enemyRB.velocity.y;
+			horizontalVelocity = enemyRB.velocity.x;
+			timeFreezeInitialization = true;
+			// shuts off all movement
+			enemyRB.gravityScale = 0;
+			enemyRB.velocity = new Vector2(0, 0);
+
+		// when time isn't frozen
+		}
+		else if (timeFreezeInitialization && !timeFreeze.timeFrozen)
+		{
+			// turns gravity back on
+			enemyRB.gravityScale = 1;
+			enemyRB.velocity = new Vector2 (horizontalVelocity, verticalVelocity);
+			// resets TF mechanism
+			timeFreezeInitialization = false;
+		}
+	}
+}
