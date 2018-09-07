1f9c5fe638825b2a0631d875ecc7fe9119e70fd3
Jared <jaredhansen2007@yahoo.com>
2018-09-07 11:00:55 -0700

    added health system to player

---
+++ Assets/scripts/playerDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDamage : MonoBehaviour {
+	// initial variables
+	public int playerStartingHealth;
+	private int playerHealth;
+
+	// makes the player alive, especially after they died
+	void lifeToPlayer()
+	{
+		// sets player health to starting health
+		playerHealth = playerStartingHealth;
+		// sets the player to active if it hasn't already
+		gameObject.SetActive (true);
+	}
+
+	// Use this for initialization
+	void Start () {
+		lifeToPlayer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerHealth <= 0) 
+		{
+			// death sequence
+			gameObject.SetActive(false);
+		}
+	}
+}
